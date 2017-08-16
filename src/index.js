38ef96b7b973bdfe96d49e68fee2e92be632aee2
Matthew Jaikaran <mattjaikaran@gmail.com>
2017-08-15 20:48:09 -0400

    Implemented youtube api and react logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
-// Create a new component. This component should produce HTML
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDpCxjPaywHmqosEmdCw1ESJ4o11FhFRoI';
 
-Const App = function() {
-  return <div>Hi!</div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+    this.videoSearch('portugal the man')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 250 )
+
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
+
+      </div>
+    );
+  }
 }
-// Take component's HTML in the DOM
+
+ReactDOM.render(<App />, document.querySelector('.container'));
